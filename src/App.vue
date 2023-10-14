3f1eccd78d21097dae7cfbf7e75a22c08ca8e05d
Josh Gilheany <113118032+JGilheany@users.noreply.github.com>
2023-10-14 23:42:11 +0100

    14.10.23 4

--- src/App.vue
+++ src/App.vue
@@ -114,44 +114,36 @@ export default {
           "mech": "Bertha"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "Eagle",
+          "alias": "John War II",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Necrophile"
+          "mech": "Bob Battle"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
+          "callsign": "Sloth",
+          "alias": "Of Course I Still Love You!",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "mech": "Messiah"
         },
         {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
+          "callsign": "Trojan",
+          "alias": "Rod Silver",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Mayfly"
+          "mech": "Download More RAM"
         },
         {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
+          "callsign": "Sekhmet",
+          "alias": "SEKHMET-K4",
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Rio Bravo"
-        },
-        {
-          "callsign": "Arc",
-          "alias": 'Arcee Day',
-          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Numbnuts"
+          "mech": "Khopesh"
         },
 
       ],
